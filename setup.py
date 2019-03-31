61502b1c15710ceb0fc04e490be73dfe8ae86881
J. Scott Moreland <morelandjs@gmail.com>
2019-03-31 16:52:55 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Melo NCAA Basketball',
+      version='0.1',
+      description='Melo NCAA Basketball ratings and predictions',
+      author='J. Scott Moreland',
+      author_email='morelandjs@gmail.com',
+      py_modules = ['melo_ncaabb']
+      )
